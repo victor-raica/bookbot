0d3ef130c6673c902d579c4e8098642c1de18c41
Victor R <victor.raica.86@gmail.com>
2025-10-29 18:38:25 +0200

    Before report generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_chars
 
 
 def get_book_text(path):
@@ -10,7 +11,9 @@ def main():
     book_path = './books/frankenstein.txt'
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
+    chars_count = count_chars(book_text)
     print(f'Found {num_words} total words')
+    print(chars_count)
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_chars(text):
+    count = {}
+    for char in text:
+        char = char.lower()
+        if char not in count:
+            count[char] = 1
+        else:
+            count[char] += 1
+    return count
